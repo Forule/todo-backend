20814f93e87e6ce3ee0c2d8affbef0ae3d35bbec
Jan Muljowin <janmuljowin@gmail.com>
2026-03-02 00:32:15 +0100

    initial patch post delete save and load Todos

--- server.js
+++ server.js
@@ -1,18 +1,88 @@
 const express = require('express');
 
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
+app.use(express.json())
 
-let todos = ["Bier", "Lernen"]
+const DATA_FILE = path.join(__dirname, 'todos.json');
+
+function loadTodos() {
+
+    try{
+
+        if (!fs.existsSync(DATA_FILE)) return []
+        const data = fs.readFileSync(DATA_FILE, 'utf-8')
+        return JSON.parse(data)
+
+    }catch (error){
+        console.error("Fehler beim Laden", error)
+        return []
+    }
+
+}
+
+function saveTodos(data) {
+
+    try{
+
+        fs.writeFileSync(DATA_FILE, JSON.stringify(data, null,2))
+
+    }catch(error){
+
+        console.error("Fehler beim Speichern", error)
+
+    }
+
+}
+
+let todos = loadTodos()
 
 app.get('/todos', (req, res) =>{
 
     res.json(todos);
 
 });
+
+app.post('/todos', (req, res) => {
+    const newTodo = req.body;
+    todos.push(newTodo);
+    saveTodos(todos)
+    res.status(201).json(newTodo);
+});
+
+app.delete('/todos/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    todos = todos.filter(t => String(t.id) !== id)
+    saveTodos(todos)
+
+    res.status(204).send()
+
+})
+
+app.patch('/todos/:id', (req, res) => {
+    
+    const { id } = req.params
+
+    const todo = todos.find(t => String(t.id) === id)
+
+    if (todo) {
+
+        todo.completed = !todo.completed
+        saveTodos(todos)
+        res.json(todo)
+
+    }else {
+        res.status(404).json({ message: "Todo nicht gefunden" });
+    }
+
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
