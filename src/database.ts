07cac43d5fc5b47053ed5ef637c5a85955391396
Jan Muljowin <janmuljowin@gmail.com>
2026-03-02 20:16:59 +0100

    added database

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata"
+import {DataSource} from "typeorm"
+
+export const database: DataSource = new DataSource({
+
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "devuser",
+    password: "devpass",
+    database: "todo",
+
+})
+
+
