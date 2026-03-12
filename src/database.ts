297a97a385fb4cc3b3019245ef0d8bd84b76e752
Jan Muljowin <janmuljowin@gmail.com>
2026-03-12 20:19:36 +0100

    database changes

--- src/database.ts
+++ src/database.ts
@@ -6,11 +6,11 @@ import { User } from "./entities/User.js"
 export const database: DataSource = new DataSource({
 
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "devuser",
-    password: "devpass",
-    database: "todo",
+    host: process.env.DATABASE_HOST || "localhost",
+    port: Number (process.env.DATABASE_PORT) || 5432,
+    username: process.env.DATABASE_USERNAME || "devuser",
+    password: process.env.DATABASE_PASSW || "devpass",
+    database: process.env.DATABASE_NAME || "todo",
     synchronize: true,
     entities: [Todo, User],
     connectTimeoutMS: 5000
